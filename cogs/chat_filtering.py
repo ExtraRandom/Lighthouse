3faf8483de1fcb6467b10a8c8ed04e82008a29d3
Extra_Random <crazyblob11@gmail.com>
2019-05-18 05:00:12 +0100

    Added new mode to token stfu which deletes all messages

--- cogs/chat_filtering.py
+++ cogs/chat_filtering.py
@@ -16,18 +16,25 @@ class ChatFilter:
                           "destroy", "overthrow"]
         self.bad_words_temp = []
         self.token_stfu_warn_time = None
+        self.token_stfu_all = False
 
-    @commands.command(aliases=['stfutoken', "tstfu"], hidden=True, name="tokenstfu")
+    @commands.command(name="tokenstfu", hidden=True, aliases=['stfutoken', "tstfu"])
     @perms.is_token_stfu_controller()
     async def token_stfu_toggle_command(self, ctx):
         """Toggles Auto-Deletion of Token messages with bad words"""
         self.token_stfu = not self.token_stfu
         await ctx.send("Token STFU is now {}".format(self.token_stfu))
 
-    @commands.command(hidden=True, name="tokenadd", aliases=['tokenstfuadd', 'stfuadd', 'badword'])
+    @commands.command(name="tokenmode", hidden=True, aliases=["modetoken", "stfumode"])
+    @perms.is_token_stfu_controller()
+    async def token_stfu_mode_toggle(self, ctx):
+        self.token_stfu_all = not self.token_stfu_all
+        await ctx.send("Token STFU - Delete all msgs - is now {}".format(self.token_stfu))
+
+    @commands.command(name="tokenadd", hidden=True, aliases=['tokenstfuadd', 'stfuadd', 'badword'])
     @perms.is_token_stfu_controller()
     async def token_stfu_add_temp_word(self, ctx, *, word: str):
-        """Add temporary word to Token bads word list
+        """Add temporary word to Token bad word list
 
         I'll add a better solution than this in the future, if still needed"""
         if word in self.bad_words_temp or word in self.bad_words:
@@ -57,19 +64,22 @@ class ChatFilter:
         if msg.author.id == 328310056754085888:  # 92562410493202432:   # id for testing
             if msg.channel.id in [575848857926107136, 223132558609612810]:  # TODO add other channels to block
                 if self.token_stfu is True:
-                    msg_fmt = str(msg.clean_content).lower()
-                    if msg_fmt in self.bad_words or msg_fmt in self.bad_words_temp:
+                    if self.token_stfu_all is True:
                         await msg.delete()
+                    else:
+                        msg_fmt = str(msg.clean_content).lower()
+                        if msg_fmt in self.bad_words or msg_fmt in self.bad_words_temp:
+                            await msg.delete()
 
-                        if self.token_stfu_warn_time is not None:
-                            last_warn_time = datetime.now() - self.token_stfu_warn_time
-                            if last_warn_time.seconds >= 20:
+                            if self.token_stfu_warn_time is not None:
+                                last_warn_time = datetime.now() - self.token_stfu_warn_time
+                                if last_warn_time.seconds >= 20:
+                                    await msg.channel.send("{} STFU!".format(msg.author.mention))
+                                    self.token_stfu_warn_time = datetime.now()
+
+                            elif self.last_token_msg_time is None:
                                 await msg.channel.send("{} STFU!".format(msg.author.mention))
                                 self.token_stfu_warn_time = datetime.now()
-
-                        elif self.last_token_msg_time is None:
-                            await msg.channel.send("{} STFU!".format(msg.author.mention))
-                            self.token_stfu_warn_time = datetime.now()
 
     async def invite_blocker(self, msg):
         """https://discord.gg/"""
